508d5ec447067638cf32d65c9bb5b71c9c039fdc
hvasyak-bit <hvasyak@gmail.com>
2025-10-06 20:35:56 +0400

    Update __init__.py

--- hikka/__init__.py
+++ hikka/__init__.py
@@ -7,10 +7,10 @@
 
 # Do not delete this file, it will cause errors.
 
-__author__ = "Dan Gazizullin"
-__contact__ = "me@hikariatama.ru"
-__copyright__ = "Copyright 2022, Dan Gazizullin"
-__credits__ = ["LonamiWebs", "penn5"]
-__license__ = "AGPLv3"
+__author__ = "Kerdik"
+__contact__ = "kerdikproject@gmail.com"
+__copyright__ = "Copyright 2025, Kerdik"
+__license__ = "MIT"
 __maintainer__ = "developer"
 __status__ = "Production"
+
